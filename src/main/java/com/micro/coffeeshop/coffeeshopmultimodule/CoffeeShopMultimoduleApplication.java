a08ded06ff8232ae7de327b774c4aba5c18beeb4
BijeshChandran <intruder>
2019-08-02 16:37:53 +0530

    first comiot

---
+++ src/main/java/com/micro/coffeeshop/coffeeshopmultimodule/CoffeeShopMultimoduleApplication.java
@@ -0,0 +1,13 @@
+package com.micro.coffeeshop.coffeeshopmultimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopMultimoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeShopMultimoduleApplication.class, args);
+    }
+
+}
